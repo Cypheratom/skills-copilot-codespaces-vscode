df19f163e242578b129ffadd37d0f175fe1174e6
Cypheratom <142213534+Cypheratom@users.noreply.github.com>
2023-08-14 09:32:23 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -3,10 +3,9 @@ function skillMembers() {
     restrict: 'E',
     templateUrl: 'views/skill-members.html',
     controller: 'SkillMembersController',
-    controllerAs: 'skillMembersCtrl',
+    controlerAs: 'vm',
     bindToController: true,
     scope: {
-      skill: '=',
       members: '='
     }
   };
